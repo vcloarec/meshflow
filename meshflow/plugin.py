c2aa890d3e9821e5575007be3d7dfd4452fbc781
vcloarec <vcloarec@gmail.com>
2023-12-12 19:54:44 -0400

    add visibilty action in mesh menu

--- meshflow/plugin.py
+++ meshflow/plugin.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel, QMenu, QAction
+from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 
 from qgis.gui import QgsDockWidget
@@ -12,10 +13,16 @@ class MeshFlowPlugin:
         self._iface = iface
 
         self._dock_widget = None
+        self._visibility_action = None
 
     def initGui(self):
         self._dock_widget = MeshFlowDockWidget(self._iface)
+        self._visibility_action = QAction("Mesh Flow", self._iface.mainWindow())
         self._iface.mainWindow().addDockWidget(Qt.LeftDockWidgetArea, self._dock_widget)
+        self._dock_widget.setToggleVisibilityAction(self._visibility_action)
+
+        mesh_menu = self._iface.mainWindow().findChild(QMenu, "mMeshMenu")
+        mesh_menu.addAction(self._visibility_action)
 
     def unload(self):
         self._dock_widget.deleteLater()
